df10b322892578cb8e75750099ecf059472cde98
ldody <leo.dody01@gmail.com>
2025-04-07 13:48:00 +0200

    Update FOB_prepro.bash

--- slurm/FOB_prepro.bash
+++ slurm/FOB_prepro.bash
@@ -5,7 +5,7 @@
 #SBATCH --mem-per-cpu=6000
 #SBATCH --mail-type=all
 #SBATCH --mail-user=leo.dody1@univ-lyon3.fr
-#SBATCH --output=FOB_prepro_%a.out#/dev/null
+#SBATCH --output=/dev/null #FOB_prepro_%a.out#/dev/null
 #SBATCH --job-name=FOB_prepro_%A_%a
 #SBATCH --partition=c6420-ib100
 #SBATCH --array=0-250%20
