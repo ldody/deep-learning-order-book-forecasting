dae4fe6f7880bc85a919dc46e850c59cd5a4d656
ldody <leo.dody01@gmail.com>
2025-04-22 18:14:23 +0200

    Create opti.bash

---
+++ slurm/opti.bash
@@ -0,0 +1,23 @@
+#!/bin/sh
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=4000
+#SBATCH --mail-type=all
+#SBATCH --mail-user=leo.dody1@univ-lyon3.fr
+#SBATCH --output=opti_%a.out #FOB_prepro_%a.out#/dev/null
+#SBATCH --job-name=opti_%A_%a
+#SBATCH --partition=c6420-ib100
+#SBATCH --array=0-3%4
+
+
+module purge
+module use /easybuild/AlmaLinux/8/skylake-avx512/mlxln5.5/foss2022b/modules/all
+module load Python/3.10.4-GCCcore-12.2.0
+source /home_nfs/polytech/leo.dody/PhD/Article_1/PhD_article_1/.venv/bin/activate
+
+export PYTHONUNBUFFERED=TRUE
+
+python3 ../src/model/model_main.py -ba True --job_id $SLURM_ARRAY_TASK_ID
+
+deactivate
