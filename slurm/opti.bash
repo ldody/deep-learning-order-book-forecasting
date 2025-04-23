b4f83078c3aebcb1cd4fd6d20edbe6fbfcfc6f80
ldody <leo.dody01@gmail.com>
2025-04-23 18:05:55 +0200

    Update opti.bash

--- slurm/opti.bash
+++ slurm/opti.bash
@@ -5,10 +5,10 @@
 #SBATCH --mem-per-cpu=4000
 #SBATCH --mail-type=all
 #SBATCH --mail-user=leo.dody1@univ-lyon3.fr
-#SBATCH --output=opti_%a.out #FOB_prepro_%a.out#/dev/null
+#SBATCH --output=/dev/null #FOB_prepro_%a.out#/dev/null
 #SBATCH --job-name=opti_%A_%a
 #SBATCH --partition=c6420-ib100
-#SBATCH --array=0-3%4
+#SBATCH --array=0-399%30
 
 
 module purge
