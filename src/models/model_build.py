509e9cf32f659b976eef5157c54b9c6c30898352
ldody <leo.dody01@gmail.com>
2025-04-23 16:36:41 +0200

    Update model_build.py

--- src/models/model_build.py
+++ src/models/model_build.py
@@ -126,7 +126,7 @@ class ANN_model():
 
 	def sign_accuracy(self, y_true, y_pred):
 		# Compare les signes : True si les signes sont identiques
-		y_pred = tf.where(tf.math.is_nan(y_pred), tf.zeros_like(y_pred), y_pred)
+		y_pred = tf.where(tf.math.is_nan(y_pred), tf.zeros_like(y_pred, dtype=tf.float32), y_pred)
 		same_sign = tf.equal(tf.sign(y_true), tf.sign(y_pred))
 	
 		return tf.reduce_mean(tf.cast(same_sign, tf.float32))
