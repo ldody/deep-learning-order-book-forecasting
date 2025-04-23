57bfd1a561d0d7b6fb1971a94cf3ddc997312263
ldody <leo.dody01@gmail.com>
2025-04-23 17:25:06 +0200

    Update model_main.py

--- src/models/model_main.py
+++ src/models/model_main.py
@@ -133,6 +133,7 @@ class Regression(Base):
 			
 			# preparing datasets
 			self.load_data()
+			print(self.features.dtypes)
 			X_data, Y_data, _, _, _ = self.prepro.preprocessing(self.df_features, data_type = self.to_process['data'])
 			X_data, Y_data = X_data[:int(0.8*len(X_data))], Y_data[:int(0.8*len(Y_data))]
 			
