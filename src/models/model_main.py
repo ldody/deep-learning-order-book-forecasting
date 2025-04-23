e6a3842ad11209f07e8d3d8e81d2443c3ba5372f
ldody <leo.dody01@gmail.com>
2025-04-23 15:50:17 +0200

    Update model_main.py

--- src/models/model_main.py
+++ src/models/model_main.py
@@ -109,7 +109,7 @@ class Regression(Base):
 					sys.exit('All optimizations performed')
 				
 				else:
-					self.to_process = df_assets.loc[df_assets['optimization'] != False].iloc[0]
+					self.to_process = df_assets.loc[df_assets['optimization'] != True].iloc[0]
 					STUDY_NAME, DB_PATH, storage = load_optuna_config()
 				
 				try:
