09637dc6b380d9ad2f1e5a45c63a6819c4d92dff
ldody <leo.dody01@gmail.com>
2025-06-24 16:38:18 +0200

    Create GSA.bash

---
+++ slurm/GSA.bash
@@ -0,0 +1,23 @@
+#!/bin/sh
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --mem-per-cpu=6000
+#SBATCH --mail-type=all
+#SBATCH --mail-user=leo.dody1@univ-lyon3.fr
+#SBATCH --output=GSA_%a.out
+#SBATCH --job-name=GSA_%a
+#SBATCH --partition=c6420-ib100
+#SBATCH --array=0-100%3
+
+
+module purge
+module use /easybuild/AlmaLinux/8/skylake-avx512/mlxln5.5/foss2022b/modules/all
+module load Python/3.10.4-GCCcore-12.2.0
+source /home_nfs/polytech/leo.dody/PhD/Article_1/PhD_article_1/.venv/bin/activate
+
+export PYTHONUNBUFFERED=TRUE
+
+python3 ../src/GSA/GSA_main.py
+
+deactivate
