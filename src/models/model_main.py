f920bbf383b81b7c5d9685435b8b827f4f44cd92
ldody <leo.dody01@gmail.com>
2025-06-30 14:40:20 +0200

    maj

--- src/models/model_main.py
+++ src/models/model_main.py
@@ -247,7 +247,7 @@ class Regression(Base):
 			X_data, Y_data, _, _, _ = self.prepro.preprocessing(self.df_features, data_type = self.to_process['data'])
 			X_data, Y_data = X_data[:int(0.8*len(X_data))], Y_data[:int(0.8*len(Y_data))]
 			
-			k = 5
+			k = 3#5
 			kf = KFold(n_splits=k, shuffle=True, random_state=42)
 			score = []
 
